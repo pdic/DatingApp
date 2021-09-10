6acf9f764976f9d4498b5b4a854a18288575a449
Peter Dic <peter.dic@siemens-healthineers.com>
2021-09-10 20:54:39 +0200

    sec2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
